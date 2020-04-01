34e73f2e5d76cd7722da1728d7f5bef68c204d9c
almuz <almuz@inbox.lv>
2020-04-01 11:33:21 +0300

    CSS loader

--- config/webpack-dev.js
+++ config/webpack-dev.js
@@ -1,16 +1,31 @@
-const path = require("path")
+const path = require('path')
 
 module.exports = {
-    entry: {
-        main: "./src/index.js"
-    },
-    mode: "development",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, '../dist'),
-        publicPath: "/"
-    },
-    devServer: {
-        contentBase: "dist"
-    }
+	entry: {
+		main: './src/index.js'
+	},
+	mode: 'development',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, '../dist'),
+		publicPath: '/'
+	},
+	devServer: {
+		contentBase: 'dist'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'style-loader'
+					},
+					{
+						loader: 'css-loader'
+					}
+				]
+			}
+		]
+	}
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log('Webpack works');
+require('./main.css')
