45bdd9d439ad1eda0d50dd96fa42ffa62a9da160
almuz <almuz@inbox.lv>
2020-04-06 15:13:00 +0300

    Files and changes for production

--- src/server/express.js
+++ src/server/express.js
@@ -3,23 +3,28 @@ import path from 'path'
 
 const server = express()
 
-const webpack = require('webpack')
-const config = require('../../config/webpack-dev')
-
-const compiler = webpack(config)
-
-const webpackHotMiddleware = require('webpack-hot-middleware')(compiler)
-const webpackDevMiddleware = require('webpack-dev-middleware')(
-	compiler,
-	config.devServer
-)
-
-server.use(webpackDevMiddleware)
-server.use(webpackHotMiddleware)
+const isProd = process.env.NODE_ENV === "production"
+if (!isProd) {
+	const webpack = require('webpack')
+	const config = require('../../config/webpack-dev')
+	
+	const compiler = webpack(config)
+	
+	const webpackHotMiddleware = require('webpack-hot-middleware')(compiler)
+	const webpackDevMiddleware = require('webpack-dev-middleware')(
+		compiler,
+		config.devServer
+	)
+	
+	server.use(webpackDevMiddleware)
+	server.use(webpackHotMiddleware)
+}
 
 const staticMiddleware = express.static('dist')
 server.use(staticMiddleware)
 
-server.listen(8080, () => {
-	console.log('Server is listening')
+const PORT = process.env.PORT || 8080
+
+server.listen(PORT, () => {
+	console.log('Server is listening on ', PORT)
 })
