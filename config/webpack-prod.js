43fc1558fc742467cc38b28c6afe5c1365944b1b
almuz <almuz@inbox.lv>
2020-04-08 15:52:50 +0300

    more plugins for make build smaller

--- config/webpack-prod.js
+++ config/webpack-prod.js
@@ -3,6 +3,10 @@ const webpack = require('webpack')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const MinifyPlugin = require('babel-minify-webpack-plugin')
+const UglifyJSplugin = require('uglifyjs-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const BrotliPlugin = require('brotli-webpack-plugin')
 
 module.exports = {
 	entry: {
@@ -60,12 +64,23 @@ module.exports = {
 	plugins: [
 		new OptimizeCssAssetsPlugin(),
 		new MiniCSSExtractPlugin({
-			filename: "[name]-[contenthash].css"
+			filename: '[name]-[contenthash].css'
 		}),
 		new HTMLWebpackPlugin({
 			template: './src/index.ejs',
 			inject: true,
 			title: 'Webpack-course'
-		})
+		}),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production')
+			}
+		}),
+		new MinifyPlugin(),
+		new UglifyJSplugin(),
+		new CompressionPlugin({
+			algorithm: 'gzip'
+		}),
+		new BrotliPlugin()
 	]
 }
